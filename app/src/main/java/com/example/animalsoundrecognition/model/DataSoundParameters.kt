a860bd3c6a3cb9f659b1aeb01c66a1435555aed0
NeedThisAccount <keruk33@gmail.com>
2021-11-28 19:32:13 +0100

    most basic class avg works

--- app/src/main/java/com/example/animalsoundrecognition/model/DataSoundParameters.kt
+++ app/src/main/java/com/example/animalsoundrecognition/model/DataSoundParameters.kt
@@ -1,6 +1,6 @@
 package com.example.animalsoundrecognition.model
 
-class DataSoundParameters(val signalEnvelope:List<Double>, val rootMeanSquareEnergy:List<Double>, val zeroCrossingDensity:Double) {
+class DataSoundParameters(val typeName:String, val signalEnvelope:List<Double>, val rootMeanSquareEnergy:List<Double>, val zeroCrossingDensity:Double) {
     override fun toString(): String {
         return "Params(zeroCrossingDensity=$zeroCrossingDensity)"
     }
